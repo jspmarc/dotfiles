a8bc5f8aa8fc470c0aab857ed19ac40c42cc6660
Josep Marcello <jspmarcello@live.com>
2021-01-28 14:06:59 +0700

    removed un-neede files, adds new wallpapers

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#f1efee", "#1b1918" },
-	[SchemeSel] = { "#f1efee", "#f22c40" },
-	[SchemeOut] = { "#f1efee", "#3d97b8" },
-};
