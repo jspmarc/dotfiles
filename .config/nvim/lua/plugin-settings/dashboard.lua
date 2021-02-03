3399cc0c59f112c7c8f303ad3b95ed29d70e38dc
Josep Marcello <jspmarcello@live.com>
2021-02-04 01:23:55 +0700

    fix: fixes and changes to settings and adds new settings
    1. adds java settings for java coc-settings
    2. makes chika look prettier on dashboard-nvim
    3. fixes dashboard-nvim keybindings
    4. adds vimspector defaults
    5. adds a new alias for zsh

--- .config/nvim/lua/plugin-settings/dashboard.lua
+++ .config/nvim/lua/plugin-settings/dashboard.lua
@@ -3,16 +3,16 @@ vim.g.dashboard_default_executive  = 'fzf'
 vim.g.dashboard_custom_header = {
     '',
     '',
-    '        ⢀⣴⡾⠃⠄⠄⠄⠄⠄⠈⠺⠟⠛⠛⠛⠛⠻⢿⣿⣿⣿⣿⣶⣤⡀  ',
-    '      ⢀⣴⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣸⣿⣿⣿⣿⣿⣿⣿⣷ ',
-    '     ⣴⣿⡿⡟⡼⢹⣷⢲⡶⣖⣾⣶⢄⠄⠄⠄⠄⠄⢀⣼⣿⢿⣿⣿⣿⣿⣿⣿⣿ ',
-    '    ⣾⣿⡟⣾⡸⢠⡿⢳⡿⠍⣼⣿⢏⣿⣷⢄⡀⠄⢠⣾⢻⣿⣸⣿⣿⣿⣿⣿⣿⣿ ',
+    '        ⢀⣴⡾⠃     ⠈⠺⠟⠛⠛⠛⠛⠻⢿⣿⣿⣿⣿⣶⣤⡀  ',
+    '      ⢀⣴⣿⡿⠁              ⣸⣿⣿⣿⣿⣿⣿⣿⣷ ',
+    '     ⣴⣿⡿⡟⡼⢹⣷⢲⡶⣖⣾⣶⢄     ⢀⣼⣿⢿⣿⣿⣿⣿⣿⣿⣿ ',
+    '    ⣾⣿⡟⣾⡸⢠⡿⢳⡿⠍⣼⣿⢏⣿⣷⢄⡀ ⢠⣾⢻⣿⣸⣿⣿⣿⣿⣿⣿⣿ ',
     '  ⣡⣿⣿⡟⡼⡁⠁⣰⠂⡾⠉⢨⣿⠃⣿⡿⠍⣾⣟⢤⣿⢇⣿⢇⣿⣿⢿⣿⣿⣿⣿⣿ ',
     ' ⣱⣿⣿⡟⡐⣰⣧⡷⣿⣴⣧⣤⣼⣯⢸⡿⠁⣰⠟⢀⣼⠏⣲⠏⢸⣿⡟⣿⣿⣿⣿⣿⣿ ',
-    ' ⣿⣿⡟⠁⠄⠟⣁⠄⢡⣿⣿⣿⣿⣿⣿⣦⣼⢟⢀⡼⠃⡹⠃⡀⢸⡿⢸⣿⣿⣿⣿⣿⡟ ',
-    ' ⣿⣿⠃⠄⢀⣾⠋⠓⢰⣿⣿⣿⣿⣿⣿⠿⣿⣿⣾⣅⢔⣕⡇⡇⡼⢁⣿⣿⣿⣿⣿⣿⢣ ',
-    ' ⣿⡟⠄⠄⣾⣇⠷⣢⣿⣿⣿⣿⣿⣿⣿⣭⣀⡈⠙⢿⣿⣿⡇⡧⢁⣾⣿⣿⣿⣿⣿⢏⣾ ',
-    ' ⣿⡇⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢻⠇⠄⠄⢿⣿⡇⢡⣾⣿⣿⣿⣿⣿⣏⣼⣿ ',
+    ' ⣿⣿⡟⠁ ⠟⣁ ⢡⣿⣿⣿⣿⣿⣿⣦⣼⢟⢀⡼⠃⡹⠃⡀⢸⡿⢸⣿⣿⣿⣿⣿⡟ ',
+    ' ⣿⣿⠃ ⢀⣾⠋⠓⢰⣿⣿⣿⣿⣿⣿⠿⣿⣿⣾⣅⢔⣕⡇⡇⡼⢁⣿⣿⣿⣿⣿⣿⢣ ',
+    ' ⣿⡟  ⣾⣇⠷⣢⣿⣿⣿⣿⣿⣿⣿⣭⣀⡈⠙⢿⣿⣿⡇⡧⢁⣾⣿⣿⣿⣿⣿⢏⣾ ',
+    ' ⣿⡇ ⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢻⠇  ⢿⣿⡇⢡⣾⣿⣿⣿⣿⣿⣏⣼⣿ ',
     ' ⣿⣷⢰⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢰⣧⣀⡄⢀⠘⡿⣰⣿⣿⣿⣿⣿⣿⠟⣼⣿⣿ ',
     ' ⢹⣿⢸⣿⣿⠟⠻⢿⣿⣿⣿⣿⣿⣿⣿⣶⣭⣉⣤⣿⢈⣼⣿⣿⣿⣿⣿⣿⠏⣾⣹⣿⣿ ',
     ' ⢸⠇⡜⣿⡟⠄⠄⠄⠈⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟⣱⣻⣿⣿⣿⣿⣿⠟⠁⢳⠃⣿⣿⣿ ',
@@ -26,7 +26,7 @@ vim.g.dashboard_custom_shortcut = {
   last_session       = 'LDR s l',
   find_history       = 'LDR f h',
   find_file          = 'LDR f f',
-  new_file           = 'LDR c n',
+  new_file           = 'CTRL  n',
   change_colorscheme = 'LDR f c',
   find_word          = 'LDR f r',
   book_marks         = 'LDR f b',
@@ -34,6 +34,6 @@ vim.g.dashboard_custom_shortcut = {
 
 ----- keybindings -----
 -- session
-map('n', '<leader>ss', ':SessionSave')
-map('n', '<leader>sl', ':SessionLoad')
-map('n', '<leader>cn', ':DashboardNewFile')
+map('n', '<leader>ss', ':SessionSave<CR>')
+map('n', '<leader>sl', ':SessionLoad<CR>')
+map('n', '<C-n>', ':DashboardNewFile<CR>')
