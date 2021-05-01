a14541a3496d682fbf965920e1ff6b2cd7546991
Josep Marcello <jspmarcello@live.com>
2021-05-01 22:53:35 +0700

    fix installation script

--- automation/install-vim-plug.sh
+++ automation/install-vim-plug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bash -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs
+bash -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
