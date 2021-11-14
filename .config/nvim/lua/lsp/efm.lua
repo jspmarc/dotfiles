ea33ada9c1df7d1882c40c25db123b2a6069dd24
Josep Marcello <jspmarcello@live.com>
2021-11-15 00:41:20 +0700

    update formatter

--- .config/nvim/lua/lsp/efm.lua
+++ .config/nvim/lua/lsp/efm.lua
@@ -1,17 +1,9 @@
 -- taken from: https://github.com/tsuyoshicho/vim-efm-langserver-settings
 -- modified by github.com/jspmarc
-local eslint = {
-	lintCommand = "eslint -f unix --stdin --stdin-filename ${INPUT}",
-	lintIgnoreExitCode = true,
-	lintStdin = true,
-	lintFormats = { "%f:%l:%c: %m" },
-	formatCommand = "eslint --fix-to-stdout --stdin --stdin-filename=${INPUT}",
-	formatStdin = true,
-}
-local clang_format = { formatCommand = "clang-format -style=LLVM ${INPUT}", formatStdin = true }
-local prettier = { formatCommand = "prettier --stdin-filepath ${INPUT}", formatStdin = true }
-local stylua = { formatCommand = "stylua -s -", formatStdin = true }
-local flake8 = { formatCommand = "flake8 --quiet -", formatStdin = true }
+local clang_format = { formatCommand = 'clang-format -style=LLVM ${INPUT}', formatStdin = true }
+local prettier = { formatCommand = 'prettier --stdin-filepath ${INPUT}', formatStdin = true }
+local stylua = { formatCommand = 'stylua -s -', formatStdin = true }
+local flake8 = { formatCommand = 'flake8 --quiet -', formatStdin = true }
 
 return {
 	init_options = { documentFormatting = true },
@@ -21,17 +13,17 @@ return {
 			cpp = { clang_format },
 			css = { prettier },
 			html = { prettier },
-			javascript = { prettier, eslint },
-			javascriptreact = { prettier, eslint },
+			javascript = { prettier },
+			javascriptreact = { prettier },
 			json = { prettier },
 			lua = { stylua },
 			markdown = { prettier },
 			python = { flake8 },
 			scss = { prettier },
-			typescript = { prettier, eslint },
-			typescriptreact = { prettier, eslint },
+			typescript = { prettier },
+			typescriptreact = { prettier },
 			yaml = { prettier },
 		},
 	},
-	filetypes = { "lua", "python", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+	filetypes = { 'lua', 'python', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
 }
