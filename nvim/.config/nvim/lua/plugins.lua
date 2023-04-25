5d0464c82607222142bf155ab1e6120090c95cc3
Josep Marcello <jspmarcello@live.com>
2023-04-25 07:05:27 +0700

    nvim: fix nvim config

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -290,6 +290,7 @@ local M = require('packer').startup(function(use)
 		'williamboman/mason-lspconfig.nvim',
 		requires = { 'williamboman/mason.nvim' },
 		config = function()
+			require('mason').setup()
 			require('mason-lspconfig').setup({
 				ensure_installed = require('helpers').lsp_servers,
 			})
@@ -492,71 +493,7 @@ local M = require('packer').startup(function(use)
 	use({
 		'kyazdani42/nvim-tree.lua',
 		config = function()
-			local tree_cb = require('nvim-tree.config').nvim_tree_callback
-
-			require('nvim-tree').setup({
-				disable_netrw = true,
-				hijack_netrw = true,
-				open_on_setup = false,
-				ignore_ft_on_setup = {},
-				open_on_tab = false,
-				hijack_cursor = false,
-				update_cwd = false,
-				diagnostics = {
-					enable = true,
-					show_on_dirs = false,
-					icons = {
-						hint = '',
-						info = '',
-						warning = '',
-						error = '',
-					},
-				},
-				update_focused_file = {
-					enable = false,
-					update_cwd = false,
-					ignore_list = {},
-				},
-				system_open = {
-					cmd = nil,
-					args = {},
-				},
-				filters = {
-					dotfiles = false,
-					custom = {},
-					exclude = {},
-				},
-				view = {
-					width = 60,
-					hide_root_folder = false,
-					side = 'left',
-					preserve_window_proportions = true,
-					number = true,
-					signcolumn = 'yes',
-					mappings = {
-						custom_only = false,
-						list = {
-
-							{ key = 'v', cb = tree_cb('vsplit') },
-
-							{ key = 's', cb = tree_cb('split') },
-
-							{ key = 'U', cb = tree_cb('parent_node') },
-						},
-					},
-				},
-				actions = {
-					use_system_clipboard = true,
-					open_file = {
-						quit_on_open = true,
-					},
-				},
-				git = {
-					enable = true,
-					ignore = true,
-					timeout = 400,
-				},
-			})
+			require('nvim-tree').setup(require('plugins.nvim-tree'))
 		end,
 	})
 	use({
