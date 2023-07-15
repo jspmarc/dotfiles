50706da2680ea0241b9bb7d912b290c5d76726c2
Josep Marcello <jspmarcello@live.com>
2023-07-15 09:54:37 +0700

    script: update backup script

--- script/script/home_backup.sh
+++ script/script/home_backup.sh
@@ -1,65 +1,26 @@
 #!/bin/env sh
 
-# count the number of files in a directory
-DIR='/data/home-backup'
-FILECOUNT=$(find "$DIR" -name '*.tar.gz' | wc -l)
-CFG='/home/josep/.config'
+BASE_BACKUP_DIR=/mnt/data/home-backup
+BACKUP_ARCHIVE_NAME="$(date +'%y-%m-%d').tar.gz"
+BASE_LOG_DIR=${BASE_BACKUP_DIR}/log
+HOME="${HOME:1}"
 
-# Announcees a backup is being created
-notify-send "Starting backup creation for: $(date +'%d-%m-%y').tar.gz"
+notify-send "Creating a home backup..."
 
-# Backed up files:
-  # i3 configs
-  # Vscode settings.json
-  # dunst config
-  # neofetch config
-  # kitty config
-  # nvim config
-  # picom config
-  # polybar config
-  # rofi config
-  # spotify-tui configs
-  # lf config
+tar -cvz -C / -f "${BASE_BACKUP_DIR}/${BACKUP_ARCHIVE_NAME}" \
+    "${HOME}/Downloads" \
+    "${HOME}/Videos" \
+    "${HOME}/projects" \
+    "${HOME}/Pictures" \
+    "${HOME}/Documents" > "${BASE_LOG_DIR}/current_process.log"
 
-  # .config folder
-  # scripts
-  # .zshrc
-  # .vim folder
-  # .Xmodmap
-  # wal cache folder
-  # wal-theme folder
-  # zsh-files folder
-  # Documents folder
-  # Pictures folder
-
-# Creates backup
-#tar -cvzf /data/home-backup/"$(date +'%d-%m-%y')".tar.gz \
-  #"$CFG"/i3 "$CFG"/Code/User/settings.json "$CFG"/dunst \
-  #"$CFG"/neofetch "$CFG"/kitty "$CFG"/nvim "$CFG"/picom \
-  #"$CFG"/polybar "$CFG"/rofi "$HOME"/.p10k.zsh \
-  #"$HOME"/script "$HOME"/.vim "$HOME"/.Xmodmap "$HOME"/.zshrc \
-  #"$HOME"/.cache/wal "$CFG"/lf "$CFG"/spotify-tui "$HOME"/.zshenv \
-  #"$HOME"/wal-theme "$HOME"/Documents > /data/home-backup/log/current_process.log
-
-#tar -cvzf /data/home-backup/"$(date +'%d-%m-%y')".tar.gz \
-    #$CFG "$HOME"/.zshrc \
-    #"$HOME"/.cache/wal "$HOME"/zsh-files \
-    #"$HOME"/script "$HOME"/.vim "$HOME"/.Xmodmap \
-    #"$HOME"/Pictures \
-    #"$HOME"/wal-theme "$HOME"/Documents > /data/home-backup/log/current_process.log
-
-tar -cvzf /data/home-backup/"$(date +'%d-%m-%y')".tar.gz \
-    "$HOME"/Pictures \
-    "$HOME"/Documents > /data/home-backup/log/current_process.log
-
-# Announces the backup process is finished
-notify-send "Backup created."
-echo "Backup created on $(date)" >> /data/home-backup/log/backup.log
+notify-send "Backup ${BACKUP_ARCHIVE_NAME} created."
+echo "Backup ${BACKUP_ARCHIVE_NAME} CREATED on $(date)" >> "${BASE_LOG_DIR}/backup.log"
 
 # Deletes the oldest backup if more than 3 backup exists
-if [[ $FILECOUNT -gt 3 ]]; then
-    #tbd=$(find "$DIR" -name '*.tar.gz' | head -n1)
-    tbd=$(ls /data/home-backup/ -h | head -n1)
-    rm /data/home-backup/"$tbd"
-    echo "$tbd" deleted on "$(date)" >> /data/home-backup/log/backup.log
+filecount=$(find "$BASE_BACKUP_DIR" -name '*.tar.gz' | wc -l)
+if [[ ${filecount} -gt 3 ]]; then
+    tbd=$(ls ${BASE_BACKUP_DIR} -h | head -n1)
+    rm "$BASE_BACKUP_DIR/$tbd"
+    echo "Backup $tbd DELETED on $(date)" >> "${BASE_LOG_DIR}/backup.log"
 fi
