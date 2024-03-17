bbfe45b8fd03a4a934144c81fbddd6bd59932960
Josep Marcello <jspmarcello@live.com>
2024-03-17 10:51:57 +0700

    nvim: add friendly snippet

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -297,7 +297,15 @@ local M = require('packer').startup(function(use)
 			})
 		end,
 	})
-	use('L3MON4D3/LuaSnip')
+	use({
+		'L3MON4D3/LuaSnip',
+		tag = 'v2.*',
+		run = 'make install_jsregexp',
+		requires = { 'rafamadriz/friendly-snippets' },
+		config = function()
+			require('luasnip.loaders.from_vscode').lazy_load()
+		end,
+	})
 
 	---------------------------------------------------------------------------
 	---------------                     M                       ---------------
