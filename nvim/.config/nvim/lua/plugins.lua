f9212f8656170d208cbf5a9f4ee9da9e2a634c36
Josep Marcello <jspmarcello@live.com>
2024-10-18 20:40:16 +0900

    nvim: update comment plugin to use treesitter

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -140,34 +140,7 @@ local M = require('packer').startup(function(use)
 		config = function()
 			require('Comment').setup({
 				ignore = '^%s*$',
-				pre_hook = function(ctx)
-					if
-						vim.bo.filetype == 'typescriptreact'
-						or vim.bo.filetype == 'javascriptreact'
-						or vim.bo.filetype == 'php'
-						or vim.bo.filetype == 'html'
-						or vim.bo.filetype == 'svelte'
-						or vim.bo.filetype == 'vue'
-					then
-						local U = require('Comment.utils')
-
-						-- Detemine whether to use linewise or blockwise commentstring
-						local type = ctx.ctype == U.ctype.line and '__default' or '__multiline'
-
-						-- Determine the location where to calculate commentstring from
-						local location = nil
-						if ctx.ctype == U.ctype.block then
-							location = require('ts_context_commentstring.utils').get_cursor_location()
-						elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-							location = require('ts_context_commentstring.utils').get_visual_start_location()
-						end
-
-						return require('ts_context_commentstring.internal').calculate_commentstring({
-							key = type,
-							location = location,
-						})
-					end
-				end,
+				pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
 			})
 		end,
 	})
