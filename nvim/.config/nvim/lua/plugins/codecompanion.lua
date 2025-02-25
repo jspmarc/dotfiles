854b8de7539caa33b468751ff668e4b680fd34b6
Josep Marcello <jspmarcello@live.com>
2025-02-26 01:07:49 +0900

    nvim: add fidget spinner to codecompanion

--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -24,10 +24,14 @@ return {
 			},
 		},
 	},
+	init = function()
+		require('plugins.codecompanion.fidget'):init()
+	end,
 	dependencies = {
 		'github/copilot.vim',
 		'nvim-lua/plenary.nvim',
 		'nvim-treesitter/nvim-treesitter',
+		'j-hui/fidget.nvim',       -- Optional: For the fidget spinner
 		'hrsh7th/nvim-cmp',        -- Optional: For using slash commands and variables in the chat buffer
 		'nvim-telescope/telescope.nvim', -- Optional: For using slash commands
 		'folke/snacks.nvim',       -- Optional: Improves `vim.ui.select`
