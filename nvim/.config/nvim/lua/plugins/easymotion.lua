0710f8d4826b3878cc2e2b71fb72fc65fb4d77a8
Josep Marcello <jspmarcello@live.com>
2025-02-27 18:08:36 +0900

    nvim: use sneak to replace easymotion

--- nvim/.config/nvim/lua/plugins/easymotion.lua
+++ nvim/.config/nvim/lua/plugins/easymotion.lua
@@ -1,10 +1,6 @@
 return {
 	'easymotion/vim-easymotion',
 	keys = {
-		{ '<leader><leader>f', '<Plug>(easymotion-overwin-f)', noremap = false, desc = 'Easymotion find' },
-		{ '<leader><leader>w', '<Plug>(easymotion-overwin-w)', noremap = false, desc = 'Easymotion w' },
-		{ '<leader><leader>e', '<Plug>(easymotion-e)',         noremap = false, desc = 'Easymotion e' },
-		{ '<leader><leader>b', '<Plug>(easymotion-b)',         noremap = false, desc = 'Easymotion b' },
 		{ '<leader><leader>j', '<Plug>(easymotion-j)',         noremap = false, desc = 'Easymotion go down' },
 		{ '<leader><leader>k', '<Plug>(easymotion-k)',         noremap = false, desc = 'Easymotion go up' },
 	},
---
+++ nvim/.config/nvim/lua/plugins/sneak.lua
@@ -0,0 +1,11 @@
+return {
+	'justinmk/vim-sneak',
+	keys = {
+		{ 's', '<Plug>Sneak_s', mode = { 'n', 'x' }, desc = 'Sneak s' },
+		{ 'S', '<Plug>Sneak_S', mode = { 'n', 'x' }, desc = 'Sneak S' },
+		{ 'f', '<Plug>Sneak_f', mode = { 'n', 'x' }, desc = 'Sneak f' },
+		{ 'F', '<Plug>Sneak_F', mode = { 'n', 'x' }, desc = 'Sneak F' },
+		{ 't', '<Plug>Sneak_t', mode = { 'n', 'x' }, desc = 'Sneak t' },
+		{ 'T', '<Plug>Sneak_T', mode = { 'n', 'x' }, desc = 'Sneak T' },
+	},
+}
