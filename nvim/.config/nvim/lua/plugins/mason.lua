a83ccbc40a38f209218b3f3092e523284a7ce440
Josep Marcello <jspmarcello@live.com>
2025-03-02 23:04:08 +0900

    nvim: disable and cleanup a few plugins

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,5 +1,31 @@
+local lsp_servers = require('helpers').lsp_servers
+
 return {
-	enabled = require('helpers').not_vscode,
-	'williamboman/mason.nvim',
-	opts = {},
+	{
+		enabled = require('helpers').not_vscode,
+		'williamboman/mason.nvim',
+		opts = {},
+	},
+	{
+
+		enabled = require('helpers').not_vscode,
+		'williamboman/mason-lspconfig.nvim',
+		dependencies = { 'williamboman/mason.nvim', 'neovim/nvim-lspconfig' },
+		opts = {
+			ensure_installed = lsp_servers,
+		},
+	},
+	{
+		enabled = require('helpers').not_vscode,
+		'jayp0521/mason-null-ls.nvim',
+		dependencies = { 'williamboman/mason.nvim', 'jose-elias-alvarez/null-ls.nvim' },
+		opts = {
+			automatic_setup = true,
+			ensure_installed = vim.tbl_extend(
+				'force',
+				require('helpers').null_ls_formatters,
+				require('helpers').null_ls_linters
+			),
+		},
+	},
 }
--- nvim/.config/nvim/lua/plugins/treesitter-context.lua
+++ nvim/.config/nvim/lua/plugins/treesitter-context.lua
@@ -1,5 +1,5 @@
 return {
-	enabled = require('helpers').not_vscode,
+	enabled = false,
 	'nvim-treesitter/nvim-treesitter-context',
 	dependencies = 'nvim-treesitter/nvim-treesitter',
 }
