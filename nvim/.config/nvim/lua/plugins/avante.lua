230c4bea8b9fdcb18e1d54662859ab30b97b563c
Josep Marcello <jspmarcello@live.com>
2025-03-03 20:58:00 +0900

    nvim: update claude haiku used for avante

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -80,7 +80,7 @@ return {
 				__inherited_from = 'openai',
 				endpoint = 'https://openrouter.ai/api/v1',
 				api_key_name = 'OPENROUTER_API_KEY',
-				model = 'anthropic/claude-3.5-haiku-20241022',
+				model = 'anthropic/claude-3.5-haiku',
 				timeout = timeout,
 				max_tokens = 49152,
 			},
