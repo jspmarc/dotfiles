b349efd3ff23e972f24c690d4017736f1abfdfaa
Josep Marcello <jspmarcello@live.com>
2025-03-04 15:13:06 +0900

    wezterm: update wezterm renderer and fix indentation

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,15 +1,15 @@
 local wezterm = require('wezterm')
 
 local function get_appearance()
-  if wezterm.gui then
-    return wezterm.gui.get_appearance()
-  end
-  return 'Dark'
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return 'Dark'
 end
 
 local function get_color_scheme()
 	appearance = get_appearance()
-	if appearance:find 'Dark' then
+	if appearance:find('Dark') then
 		return 'Darkside'
 	else
 		return 'catppuccin-latte'
