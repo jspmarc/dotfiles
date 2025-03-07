eef6c608a5d1481625b4a6af17b911e180fb096e
Josep Marcello <jspmarcello@live.com>
2025-03-08 02:26:33 +0900

    zsh: update plugins location, add more submodules

--- zsh/zsh-files/plugins/main.zsh
+++ zsh/zsh-files/plugins/main.zsh
@@ -1,20 +1,20 @@
 # zsh-autosuggestions
-source ~/zsh-files/zsh-autosuggestions.zsh
+source ~/zsh-files/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 
 # fast-syntax-highlighting
-source ~/zsh-files/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+source ~/zsh-files/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
 
 # zsh-interactive-cd
-source ~/zsh-files/zsh-interactive-cd.zsh
+source ~/zsh-files/plugins/zsh-interactive-cd/zsh-interactive-cd.plugin.zsh
 
 # zsh-colored-man-pages
-source ~/zsh-files/zsh-colored-man-pages.zsh
+source ~/zsh-files/plugins/zsh-colored-man-pages.plugin.zsh
 
 # zsh-vi-mode
-source ~/zsh-files/zsh-vi-mode.zsh
+source ~/zsh-files/plugins/zsh-vi-mode/zsh-vi-mode.plugin.zsh
 
-# key-bindings
-source ~/zsh-files/key-bindings.zsh
+# FZF key-bindings
+source ~/zsh-files/plugins/fzf-key-bindings.zsh
 
-# completion
-source ~/zsh-files/completion.zsh
+# FZF completion
+source ~/zsh-files/plugins/fzf-completion.zsh
