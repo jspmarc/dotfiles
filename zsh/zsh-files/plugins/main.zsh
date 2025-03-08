4e6b3ee50c03bfa66b1d2ef4d711c6d83e58c8fb
Josep Marcello <jspmarcello@live.com>
2025-03-08 19:07:47 +0900

    zsh: clean up plugin definition & fix loading fzf key binding

--- zsh/zsh-files/plugins/main.zsh
+++ zsh/zsh-files/plugins/main.zsh
@@ -1,20 +1,21 @@
-# zsh-autosuggestions
-source ~/zsh-files/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+plugins=(
+  ~/zsh-files/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+  ~/zsh-files/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+  ~/zsh-files/plugins/zsh-interactive-cd/zsh-interactive-cd.plugin.zsh
+  ~/zsh-files/plugins/zsh-colored-man-pages.plugin.zsh
+  ~/zsh-files/plugins/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+  ~/zsh-files/plugins/fzf-key-bindings.zsh
+  ~/zsh-files/plugins/fzf-completion.zsh
+)
 
-# fast-syntax-highlighting
-source ~/zsh-files/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+for plugin in "${plugins[@]}"; do
+  if [[ -f $plugin ]]; then
+    source $plugin
+  fi
+done
 
-# zsh-interactive-cd
-source ~/zsh-files/plugins/zsh-interactive-cd/zsh-interactive-cd.plugin.zsh
+FZF_KEY_BINDINGS_PATH=~/zsh-files/plugins/fzf-key-bindings.zsh
 
-# zsh-colored-man-pages
-source ~/zsh-files/plugins/zsh-colored-man-pages.plugin.zsh
+# Load fzf key bindings correctly with zsh vi mode
+zvm_after_init_commands+=("[ -f $FZF_KEY_BINDINGS_PATH ] && source $FZF_KEY_BINDINGS_PATH")
 
-# zsh-vi-mode
-source ~/zsh-files/plugins/zsh-vi-mode/zsh-vi-mode.plugin.zsh
-
-# FZF key-bindings
-source ~/zsh-files/plugins/fzf-key-bindings.zsh
-
-# FZF completion
-source ~/zsh-files/plugins/fzf-completion.zsh
