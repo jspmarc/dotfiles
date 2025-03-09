4f1f41489ee7fa865c2f54275a7a1dd494c94941
Josep Marcello <jspmarcello@live.com>
2025-03-10 00:03:27 +0900

    nvim: remove refactoring.nvim

--- nvim/.config/nvim/lua/plugins/refactoring.lua
+++
@@ -1,9 +0,0 @@
-return {
-	enabled = require('helpers').not_vscode,
-	'ThePrimeagen/refactoring.nvim',
-	requires = {
-		{ 'nvim-lua/plenary.nvim' },
-		{ 'nvim-treesitter/nvim-treesitter' },
-	},
-	opts = {},
-}
