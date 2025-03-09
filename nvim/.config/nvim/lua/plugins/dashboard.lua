70a9fdd67f55bf374290866237a6cc5df6bb0d1e
Josep Marcello <jspmarcello@live.com>
2025-03-10 00:32:35 +0900

    nvim: swap dashboard with snacks

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++
@@ -1,7 +0,0 @@
-return {
-	enabled = require('helpers').not_vscode,
-	'nvimdev/dashboard-nvim',
-	event = 'VimEnter',
-	opts = {},
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
-}
--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -12,6 +12,46 @@ return {
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
 		bigfile = { enabled = true },
+		dashboard = {
+			preset = {
+				keys = {
+					{ icon = ' ', key = 'f', desc = 'Find File', action = ':Telescope find_files' },
+					{ icon = ' ', key = 'n', desc = 'New File', action = ':ene' },
+					{
+						icon = ' ',
+						key = 'g',
+						desc = 'Find Text',
+						action = ':Telescope live_grep',
+					},
+					{
+						icon = ' ',
+						key = 'r',
+						desc = 'Recent Files',
+						action = ':Telescope oldfiles',
+					},
+					{
+						icon = ' ',
+						key = 'c',
+						desc = 'Config',
+						action = ":lua require('telescope.builtin').find_files({cwd = vim.fn.stdpath('config')})",
+					},
+					{ icon = ' ', key = 's', desc = 'Restore Session', section = 'session' },
+					{
+						icon = '󰒲 ',
+						key = 'L',
+						desc = 'Lazy',
+						action = ':Lazy',
+						enabled = package.loaded.lazy ~= nil,
+					},
+					{ icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+				},
+			},
+			sections = {
+				{ section = 'header' },
+				{ section = 'startup', padding = 1 },
+				{ section = 'keys',    gap = 1 },
+			},
+		},
 		input = { enabled = true },
 		lazygit = { enabled = true },
 		notifier = { enabled = true },
