8d3630eb4fd6121d2f7fae78ad658e10e5864d8a
Josep Marcello <jspmarcello@live.com>
2025-03-19 11:56:51 +0900

    nvim: add excluded lsp clients to garbage-day

--- nvim/.config/nvim/lua/plugins/garbage-day.lua
+++ nvim/.config/nvim/lua/plugins/garbage-day.lua
@@ -4,5 +4,12 @@ return {
 	event = 'VeryLazy',
 	opts = {
 		-- your options here
+		excluded_lsp_clients = {
+			'copilot',
+			'null-ls',
+			'jdtls',
+			'marksman',
+			'lua_ls', -- see: https://github.com/Zeioth/garbage-day.nvim/commit/750ef08ae6031ee3683014c5349144340c08ead6
+		},
 	},
 }
