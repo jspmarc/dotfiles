1056541860409a4b7b1a41eef990cca0c0ae3463
Josep Marcello <jspmarcello@live.com>
2025-11-19 19:25:32 +0900

    nvim: update code companion conf

--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -8,6 +8,8 @@ return {
 		'CodeCompanionChat',
 		'CodeCompanionCmd',
 		'CodeCompanionActions',
+		'CodeCompanionHistory',
+		'CodeCompanionSummaries',
 	},
 	keys = plugin_keys.codecompanion,
 	opts = {
@@ -63,7 +65,9 @@ return {
 							model = {
 								default = 'qwen2.5-coder:3b',
 								choices = {
-									'qwen3:0.6b',
+									'qwen2.5-coder:7b',
+									'qwen2.5-coder:3b',
+									'deepseek-coder:6.7b',
 								},
 							},
 						},
@@ -116,6 +120,58 @@ return {
 				},
 			},
 		},
+		extensions = {
+			history = {
+				enabled = true,
+				opts = {
+					-- Keymap to open history from chat buffer (default: gh)
+					keymap = 'gh',
+					-- Save all chats by default (disable to save only manually using 'sc')
+					auto_save = true,
+					-- Picker interface (auto resolved to a valid picker)
+					picker = 'snacks', --- ('telescope', 'snacks', 'fzf-lua', or 'default')
+					chat_filter = function(chat_data)
+						return chat_data.cwd == vim.fn.getcwd()
+					end,
+					-- Customize picker keymaps (optional)
+					picker_keymaps = {
+						rename = { n = 'r', i = '<M-r>' },
+						delete = { n = 'd', i = '<M-d>' },
+						duplicate = { n = '<C-y>', i = '<C-y>' },
+					},
+					auto_generate_title = true,
+					title_generation_opts = {
+						adapter = 'openrouter',
+						model = 'qwen/qwen3-coder-30b-a3b-instruct',
+						-- adapter = 'ollama',
+						-- model = 'qwen2.5-coder:3b',
+						refresh_every_n_prompts = 10,
+					},
+					---When chat is cleared with `gx` delete the chat from history
+					delete_on_clearing_chat = true,
+
+					-- Summary system
+					summary = {
+						-- Keymap to generate summary for current chat (default: 'gcs')
+						create_summary_keymap = 'gcs',
+						-- Keymap to browse summaries (default: 'gbs')
+						browse_summaries_keymap = 'gbs',
+
+						generation_opts = {
+							adapter = 'openrouter',
+							model = 'qwen/qwen3-coder-30b-a3b-instruct',
+							-- adapter = 'ollama',
+							-- model = 'qwen2.5-coder:3b',
+							context_size = 90000, -- max tokens that the model supports
+							include_references = true, -- include slash command content
+							include_tool_outputs = true, -- include tool execution results
+							system_prompt = nil, -- custom system prompt (string or function)
+							format_summary = nil, -- custom function to format generated summary e.g to remove <think/> tags from summary
+						},
+					},
+				},
+			},
+		},
 	},
 	init = function()
 		require('plugins.codecompanion.fidget'):init()
@@ -141,5 +197,6 @@ return {
 		'hrsh7th/nvim-cmp', -- Optional: For using slash commands and variables in the chat buffer
 		'nvim-telescope/telescope.nvim', -- Optional: For using slash commands
 		'folke/snacks.nvim', -- Optional: Improves `vim.ui.select`
+		'ravitemer/codecompanion-history.nvim',
 	},
 }
