0d444b334fea14b769590d5a9fc74f26ae5d832f
Josep Marcello <josep.marcello@cross-border.jp>
2025-12-02 18:07:40 +0900

    nvim: update dashboard keys

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -15,6 +15,16 @@ return {
 		bigfile = { enabled = true },
 		bufdelete = { enabled = true },
 		dashboard = {
+			preset = {
+				keys = {
+					{ icon = " ", key = "p", desc = "Restore Session", action = ":lua require('session_manager').load_session()" },
+					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+					{ icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+					{ icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+					{ icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+				}
+			},
 			sections = {
 				{ section = 'header' },
 				{ section = 'startup', padding = 1 },
