b9a89c483584cf2251a06d5175db451235cf5c17
Josep Marcello <josep.marcello@cross-border.jp>
2025-12-10 11:58:32 +0900

    neovide: fix pasting on mac

--- nvim/.config/nvim/lua/keybinds/neovide.lua
+++ nvim/.config/nvim/lua/keybinds/neovide.lua
@@ -4,9 +4,14 @@ local keybinds = {}
 
 if is_mac then
 	keybinds = {
-		{ '<D-v>', '"+P',             mode = { 'n', 'v' } },
-		{ '<D-v>', '<C-R>+',          mode = { 'i', 'c' } },
-		{ '<D-v>', [[<C-\><C-N>"+P]], mode = 't' },
+		{
+			'<D-v>',
+			function()
+				vim.api.nvim_paste(vim.fn.getreg('+'), true, -1)
+			end,
+			mode = { 'n', 'v', 's', 'x', 'o', 'i', 'l', 'c', 't' },
+			{ noremap = true, silent = true },
+		},
 	}
 else
 	keybinds = {
