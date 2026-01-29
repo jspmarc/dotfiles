a7ec6c79e17c0aff9dd33f2066095b93c0c8dbbc
Josep Marcello <josep.marcello@cross-border.jp>
2026-01-29 10:15:24 +0900

    nvim: update lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -4,26 +4,58 @@ return {
 	event = { 'BufReadPost', 'BufNewFile' },
 	opts = {
 		options = {
-			disabled_filetypes = { 'dashboard', 'Avante', 'AvanteSelectedFiles', 'AvanteInput' },
+			disabled_filetypes = { 'dashboard', 'Avante', 'AvanteSelectedFiles', 'AvanteInput', 'opencode_terminal' },
 			theme = 'auto',
+		},
+		winbar = {
+			lualine_a = {
+				{
+					'filename',
+					file_status = true,
+					path = 1,
+				},
+			},
+			lualine_b = { 'location' },
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = {},
+			lualine_z = {},
+		},
+		inactive_winbar = {
+			lualine_a = {
+				{
+					'filename',
+					file_status = true,
+					path = 1,
+					shorting_target = 80,
+				},
+			},
+			lualine_b = { 'location' },
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = {},
+			lualine_z = {},
 		},
 		sections = {
 			lualine_a = { 'mode' },
 			lualine_b = { 'branch', 'diff' },
 			lualine_c = {
 				{
-					'filename',
-					file_status = true,
-					path = 1,
-				},
-				{
 					'diagnostics',
 					sources = { 'nvim_diagnostic' },
 				},
 			},
-			lualine_x = { 'encoding', 'fileformat' },
-			lualine_y = { 'filetype' },
-			lualine_z = { 'location' },
+			lualine_x = { 'searchcount', 'selectioncount' },
+			lualine_y = { 'encoding', 'fileformat' },
+			lualine_z = { 'filetype' },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = { 'filename' },
+			lualine_x = { 'location' },
+			lualine_y = {},
+			lualine_z = {},
 		},
 	},
 }
