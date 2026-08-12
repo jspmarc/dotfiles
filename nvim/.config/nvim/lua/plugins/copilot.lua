813ba08c19fe3d64b22b0d4e8714fa3432769941
Josep Marcello <josep.marcello@cross-border.jp>
2026-08-12 11:24:24 +0900

    nvim: update copilot settings

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -7,11 +7,26 @@ return {
 	opts = {
 		suggestion = {
 			auto_trigger = true,
-			hide_during_completion = false,
+			hide_during_completion = true,
 			keymap = {
 				next = '<M-]>',
 				prev = '<M-[>',
 			},
 		},
+		nes = {
+			enabled = false, -- requires copilot-lsp as a dependency
+			auto_trigger = false,
+			keymap = {
+				accept_and_goto = '<leader>p',
+				accept = false,
+				dismiss = '<Esc>',
+			},
+		},
+	},
+	requires = {
+		'copilotlsp-nvim/copilot-lsp',
+		init = function()
+			vim.g.copilot_nes_debounce = 500
+		end,
 	},
 }
